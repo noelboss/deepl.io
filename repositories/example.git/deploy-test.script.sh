6e790020b35f287ba0df32b36ea6c65e835cbbf1
Noel Bossart <me@noelboss.com>
2015-06-20 15:30:55 +0200

    Enhanced example script

--- repositories/example.git/deploy-test.script.sh
+++ repositories/example.git/deploy-test.script.sh
@@ -1,7 +1,31 @@
 #!/bin/sh
 
-LOGFILE="logs/deeplio.log"
+LOG="logs/deeplio.log"
 NOW=$(date +"%Y-%m-%d-%H%M%S")
 
 echo "SSH Deploy Script run: $NOW
-" >> $LOGFILE
+" >> $LOG
+
+## More usefull example:
+#DEEPLIO="./"
+#DEPLOY="./../deepl.io"
+#LOG="./logs/deeplio.log"
+#NOW=$(date +"%Y-%m-%d-%H%M%S")
+
+## Start Sctipt
+#echo "
+#--------------------------------------------------------
+#NEW DEPLOYMENT: $NOW" >> $LOG
+
+#git checkout master >> $LOG
+## clear untracked files
+#git clean -df >> $LOG
+## revert local changes
+#git checkout -- . >> $LOG
+#git pull >> $LOG
+
+# copy some stuff around:
+#cp $DEEPLIO/index.html $DEPLOY/
+#cp $DEEPLIO/CHANGELOG.md $DEPLOY/
+
+#echo "End Deploy." >> $LOG
