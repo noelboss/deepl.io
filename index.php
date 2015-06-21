2e4d810115f883149aaa919137b597bfd291a6cc
noelboss <n.company@me.com>
2015-06-21 20:39:31 +0200

    Release Version 0.3.0

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 namespace noelbosscom;
 
 /* ------------------------------------------------------------------------------------
- * Deepl.io – Version 0.2.0
+ * Deepl.io – Version 0.3.0
  * https://github.com/noelboss/deepl.io
  *
  * Do not change this file - use config/customisation.php for your customisations
