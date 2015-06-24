1a5f7c9fa6d287252f5c0f6c014206cbf224637e
Chris Burke <chris.burke@cakedc.com>
2015-06-24 02:53:45 -0500

    Improve copy

--- README.md
+++ README.md
@@ -1,16 +1,17 @@
 # Deepl.io
-A simple yet powerful php-tool to catch GIT Web-Hooks from GITLAB and GITHUB and deploy your projects to your server after pushing.
+
+A simple yet powerful PHP tool to catch Git webhooks from GitLab and GitHub and deploy your projects to your server after pushing.
 
 #### [Current Release: 0.3.4](https://github.com/noelboss/deepl.io/archive/0.3.4.zip)
 
 ## Features
-* Catch web-hooks from gitlab & github
+
+* Catch webhooks from GitLab & GitHub
 * Configure multiple repositories
 * Configure multiple branches per repository
-* Use your own deploy scripts, eighter php, shell or both
-* Sends confirmation mails
+* Use your own deploy scripts, either PHP, shell or both
+* Sends confirmation emails
 
 #### [Visit the official website for instructions and more information »](http://deepl.io)
-
 
 © 2015 [Noël Bossart](http://noelboss.com). Made in Switzerland.
--- app/admin/library/Index.php
+++ app/admin/library/Index.php
@@ -61,7 +61,7 @@ class Index {
 					<h3 class="text-right">Edit Deployment Jobs</h3>
 
 					<div class="list-group text-right">
-						To edit and test a deployment, click on settings on the left.
+						To edit and test a deployment, click Settings on the left.
 					</div>
 				</div>
 			</div>
@@ -117,18 +117,18 @@ class Index {
 						?>
 						<div class="form-group">
 							<label for="name<?= $i ?>" class="control-label">Configuration Name</label>
-							<input type="test" class="form-control" id="name<?= $i ?>" name="name" placeholder="Configuration Title" value="<?= $conf->project->name ?>">
+							<input type="text" class="form-control" id="name<?= $i ?>" name="name" placeholder="Configuration Title" value="<?= $conf->project->name ?>">
 						</div>
 						<div class="form-group">
 							<label for="repo<?= $i ?>" class="control-label">Repository SSH URL</label>
-							<input type="email" class="form-control" id="repo<?= $i ?>" name="repo" placeholder="Repository" value="<?= $conf->project->repository_ssh_url ?>">
+							<input type="url" class="form-control" id="repo<?= $i ?>" name="repo" placeholder="Repository" value="<?= $conf->project->repository_ssh_url ?>">
 						</div>
 						<div class="form-group">
 							<label for="branch<?= $i ?>" class="control-label">Branch</label>
-							<input type="test" class="form-control" id="branch<?= $i ?>" name="branch" placeholder="deploy/dev" value="<?= $conf->project->branch ?>">
+							<input type="text" class="form-control" id="branch<?= $i ?>" name="branch" placeholder="deploy/dev" value="<?= $conf->project->branch ?>">
 						</div>
 						<div class="form-group">
-							<label for="mail<?= $i ?>" class="control-label">e-Mail</label>
+							<label for="mail<?= $i ?>" class="control-label">Email</label>
 							<div class="input-group">
 								<span class="input-group-addon">@</span>
 								<input type="email" class="form-control" id="mail<?= $i ?>" name="mail" placeholder="nofitication@email.com" value="<?= $conf->notification->mail ?>">
