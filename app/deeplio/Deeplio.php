bfe2ff82fa3fe9579df5da1a21654aa1e1df4b7b
noelboss <n.company@me.com>
2015-07-03 00:15:51 +0200

    still late

--- app/deeplio/Deeplio.php
+++ app/deeplio/Deeplio.php
@@ -34,16 +34,13 @@ namespace noelbosscom;
 				$conffile = BASE . 'config/config.json';
 			}
 
-			$this->log($_SERVER);
+			$this->log(var_dump($_SERVER));
 
 			$this->config = json_decode( file_get_contents( $conffile ) );
 
 			if(isset($this->config->log)){
 				$this->logfile = BASE.'/'.$this->config->log;
 			}
-
-			$this->log($_SERVER);
-
 
 			$this->token = substr($_SERVER['REQUEST_URI'],1);
 
