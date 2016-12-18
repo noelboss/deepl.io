159bc66eefa609eecd05b14728b910b60ba5fa7d
Nate <natronite@gmail.com>
2016-12-18 14:27:21 +0000

    Fixed spelling mistake in ‘blogging’

--- app/deeplio/Deeplio.php
+++ app/deeplio/Deeplio.php
@@ -228,7 +228,7 @@ namespace noelbosscom;
 			$status = $success ? 'SUCCESS' : 'FAILED';
 			$lead = $success ? 'High five, your deployment was successful! If you like it, please consider
 										<a href="https://twitter.com/intent/tweet?button_hashtag=Deepl.io&text=I%20use%20Deepl.io%20do%20easily%20deploy%20my%20Git%20projects%20using%20webhooks.%20Try%20it,%20it\'s%20great!%20http://deepl.io/%20%20#Deepl.io">tweeting</a>
-										or bloging about...' : 'Your project failed to deploy. Check your configuration and deployment script and read the <a href="http://deepl.io">documentation</a> or open up a <a href="https://github.com/noelboss/deepl.io/issues/new">support issue</a>.';
+										or blogging about...' : 'Your project failed to deploy. Check your configuration and deployment script and read the <a href="http://deepl.io">documentation</a> or open up a <a href="https://github.com/noelboss/deepl.io/issues/new">support issue</a>.';
 			$this->log('Sending mail to: '.$to);
 
 			$subject = '['.$conf->project->name.'] Deployment Status: '.$status;
